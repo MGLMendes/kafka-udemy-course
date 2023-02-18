bca578b89063e80a9bb316ee0fa410e48e0efe12
MGLMendes <miguelmendes203@gmail.com>
2023-02-18 13:23:44 -0300

    add callback function to send message function

---
+++ str-producer/src/main/java/com/miguel/strproducer/resources/StringProducerResource.java
@@ -0,0 +1,27 @@
+package com.miguel.strproducer.resources;
+
+import com.miguel.strproducer.services.StringProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping(value = "/producer")
+@RequiredArgsConstructor
+public class StringProducerResource {
+
+    private final StringProducerService service;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        service.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}
---
+++ str-producer/src/main/java/com/miguel/strproducer/services/StringProducerService.java
@@ -0,0 +1,29 @@
+package com.miguel.strproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message).addCallback(  //addCallback é pra termos infomrações se deu sucesso ou falha ao produzir a mensagem
+                success -> {
+                    if (success != null) {
+                        log.info("Send message with success {}", message);
+                        log.info("Partition: {}", success.getRecordMetadata().partition());  // metodo para pegarmos a partição
+                        log.info("Offset: {}", success.getRecordMetadata().offset());  // metodo para pegarmos a offset
+                    }
+                },
+                error -> log.error("Error sending message {}", message)
+        );
+
+    }
+
+}
