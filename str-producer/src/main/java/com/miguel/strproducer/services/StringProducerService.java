31ce4640a72352e1051384544594896cc141ed19
MGLMendes <miguelmendes203@gmail.com>
2023-02-18 13:44:17 -0300

    String Consumer Created

--- str-producer/src/main/java/com/miguel/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/miguel/strproducer/services/StringProducerService.java
@@ -13,16 +13,18 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message).addCallback(  //addCallback é pra termos infomrações se deu sucesso ou falha ao produzir a mensagem
-                success -> {
-                    if (success != null) {
-                        log.info("Send message with success {}", message);
-                        log.info("Partition: {}", success.getRecordMetadata().partition());  // metodo para pegarmos a partição
-                        log.info("Offset: {}", success.getRecordMetadata().offset());  // metodo para pegarmos a offset
-                    }
-                },
-                error -> log.error("Error sending message {}", message)
-        );
+        log.info("Send message {}", message);
+        kafkaTemplate.send("str-topic", message);
+//                .addCallback(  //addCallback é pra termos infomrações se deu sucesso ou falha ao produzir a mensagem
+//                success -> {
+//                    if (success != null) {
+//                        log.info("Send message with success {}", message);
+//                        log.info("Partition: {}", success.getRecordMetadata().partition());  // metodo para pegarmos a partição
+//                        log.info("Offset: {}", success.getRecordMetadata().offset());  // metodo para pegarmos a offset
+//                    }
+//                },
+//                error -> log.error("Error sending message {}", message)
+//        );
 
     }
 
