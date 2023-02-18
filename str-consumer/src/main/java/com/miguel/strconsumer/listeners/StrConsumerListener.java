b41ce2e424698d769cc665909374efcb6f9ce3ce
MGLMendes <miguelmendes203@gmail.com>
2023-02-18 15:14:57 -0300

    Patitions defined for each groupId

--- str-consumer/src/main/java/com/miguel/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/miguel/strconsumer/listeners/StrConsumerListener.java
@@ -2,18 +2,27 @@ package com.miguel.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",  //definição do groupId
+            topicPartitions = {
+                @TopicPartition(topic = "str-topic", partitions = {"0"})  //Desse jeito definimos qual partição esse groupId irá monitorar
+            },
+            containerFactory = "strContainerFactory")  // definição do container
     public void create(String message) {
         log.info("CREATE ::: Received message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                @TopicPartition(topic = "str-topic", partitions = {"1"})
+            },
+            containerFactory = "strContainerFactory")
     public void log(String message) {
         log.info("LOG ::: Receive message {}", message);
     }
--- str-producer/src/main/java/com/miguel/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/miguel/strproducer/services/StringProducerService.java
@@ -14,17 +14,17 @@ public class StringProducerService {
 
     public void sendMessage(String message) {
         log.info("Send message {}", message);
-        kafkaTemplate.send("str-topic", message);
-//                .addCallback(  //addCallback é pra termos infomrações se deu sucesso ou falha ao produzir a mensagem
-//                success -> {
-//                    if (success != null) {
-//                        log.info("Send message with success {}", message);
-//                        log.info("Partition: {}", success.getRecordMetadata().partition());  // metodo para pegarmos a partição
-//                        log.info("Offset: {}", success.getRecordMetadata().offset());  // metodo para pegarmos a offset
-//                    }
-//                },
-//                error -> log.error("Error sending message {}", message)
-//        );
+        kafkaTemplate.send("str-topic", message)
+                .addCallback(  //addCallback é pra termos infomrações se deu sucesso ou falha ao produzir a mensagem
+                success -> {
+                    if (success != null) {
+                        log.info("Send message with success {}", message);
+                        log.info("Partition: {}", success.getRecordMetadata().partition());  // metodo para pegarmos a partição
+                        log.info("Offset: {}", success.getRecordMetadata().offset());  // metodo para pegarmos a offset
+                    }
+                },
+                error -> log.error("Error sending message {}", message)
+        );
 
     }
 
